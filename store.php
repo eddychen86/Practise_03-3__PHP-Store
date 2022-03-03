2228247c9f9b1eb8888d608a4b02aeacb156b38b
eddychen86 <eddychen101020@gmail.com>
2022-03-03 14:53:03 +0800

    Step 1: add a new store.php

--- store.php
+++ store.php
@@ -14,7 +14,16 @@ require_once("../db_connect.php");
 
   </head>
   <body>
-    
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-lg-3 mb-3">
+          <div class="card p-3">
+            <h2>Sipderman</h2>
+            <div>$ 1200</div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
