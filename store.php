34e322335f5ff3316689c2ac2b123061ef7e7ac0
eddychen86 <eddychen101020@gmail.com>
2022-03-03 14:42:37 +0800

    Step 1: add a new store.php

--- store.php
+++ store.php
@@ -4,7 +4,7 @@ require_once("../db_connect.php");
 <!doctype html>
 <html lang="zh-TW">
   <head>
-    <title>Store </title>
+    <title>Store</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,7 +14,7 @@ require_once("../db_connect.php");
 
   </head>
   <body>
-      
+    
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
